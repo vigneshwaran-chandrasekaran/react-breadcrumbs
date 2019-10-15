c3545dd8da8c74b3a98981f7684616e555fa55c1
vigneshwaranc <vigneshwaranc@wekancode.com>
2019-10-15 15:36:38 +0530

    Updates

---
+++ precache-manifest.6ca4373cd4a113f4cad91388918c1e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6053d4c33690437d6b70dfb5dc917e9",
+    "url": "/react-breadcrumbs/index.html"
+  },
+  {
+    "revision": "4d833257ae793553113c",
+    "url": "/react-breadcrumbs/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7dfd74b1a179e62c1363",
+    "url": "/react-breadcrumbs/static/js/2.45fce51d.chunk.js"
+  },
+  {
+    "revision": "4d833257ae793553113c",
+    "url": "/react-breadcrumbs/static/js/main.617b77bb.chunk.js"
+  },
+  {
+    "revision": "82ab404ba9a20fec83c0",
+    "url": "/react-breadcrumbs/static/js/runtime-main.3e7fd5e1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-breadcrumbs/static/media/logo.25bf045c.svg"
+  }
+]);
